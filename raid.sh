c0cf11f7a651c722d5373be6ca94b16d207124f7
0x7С00 <droverjunior@gmail.com>
2026-06-04 01:39:01 +0500

    Update raid.sh

--- raid.sh
+++ raid.sh
@@ -4,10 +4,19 @@
 #read -p "Пути для каждого диска: " TYPE_PASS
 #read -p "Какой RAID 0 или 5: " TYPE_RAID 
 
-mdadm --create /dev/md3 -l 0 -n 2 /dev/sdb /dev/sdc#-l ${TYPE_RAID} -n ${TYPE_DISK} ${TYPE_PASS}
+mdadm --create /dev/md3 -l 0 -n 2 /dev/sdb /dev/sdc
+#-l ${TYPE_RAID} -n ${TYPE_DISK} ${TYPE_PASS}
+
+echo "конфиг mdadm"
 mdadm --detail --scan --verbose | tee -a /etc/mdadm.conf
+
+echo "Разметка рейда"
 mkfs.ext4 /dev/md3
+
+echo "Создание директории"
 mkdir /raid 
+
+echo "Запись в fstab"
 echo "/dev/md3    /raid    ext4    defaults    0    0" >> /etc/fstab
 
 mount -a
