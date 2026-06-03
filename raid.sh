5c955e13ce46434a847e38affef000d732e22bed
0x7С00 <droverjunior@gmail.com>
2026-06-04 00:58:36 +0500

    Update raid.sh

--- raid.sh
+++ raid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mdadm --craete /dev/md3 -l 5 -n 3 /dev/sdb /dev/sdc /dev/sdd
+mdadm --craete /dev/md3 -l 0 -n 2 /dev/sdb /dev/sdc 
 mdadm --dateil --scan --verbose >> /etc/mdadm.conf 
 mkfs.ext4 /dev/md3
 echo "/dev/md0    /raid    ext4    defaults    0    0" >> /etc/fstab
