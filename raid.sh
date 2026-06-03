2f6d4806070323048defc193e3cf2d20c0b756b0
0x7С00 <droverjunior@gmail.com>
2026-06-04 01:12:25 +0500

    Update raid.sh

--- raid.sh
+++ raid.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-mdadm --craete /dev/md3 -l 0 -n 2 /dev/sdb /dev/sdc 
+
+read -p "Сколько дисков: " TYPE_DISK
+read -p "Пути для каждого диска: " TYPE_PASS
+read -p "Какой RAID 0 или 5: " TYPE_RAID 
+
+mdadm --craete /dev/md3 -l ${TYPE_RAID} -n ${TYPE_DISK} ${TYPE_PASS}
 mdadm --dateil --scan --verbose >> /etc/mdadm.conf 
 mkfs.ext4 /dev/md3
-echo "/dev/md0    /raid    ext4    defaults    0    0" >> /etc/fstab
+echo "/dev/md3    /raid    ext4    defaults    0    0" >> /etc/fstab
 mkdir /raid 
 mount -a
